bc556232d67551fc0e238d9cd91bdebc69de6c2f
Muh. Ramadhan <muh.ramadhan@email.com>
2019-09-11 03:14:51 +0700

    Update

--- ber-app/_frontend/views/v_aplikasi copy.php
+++ ber-app/_frontend/views/v_aplikasi copy.php
@@ -7,16 +7,5 @@
 
 		<div class="col-xs-6 col-sm-3 col-md-6 quick-link internships">
 
-			<a href="">
-
-				<span class="icon"></span>
-
-				<span class="title">Internships</span>
-
-			</a>
-
-		</div>
-
-		-->
 
 </div>
