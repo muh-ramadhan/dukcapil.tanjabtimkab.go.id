2f2cff7544711b4cbf1a49731cc5034d6e9944d3
Muh. Ramadhan <muh.ramadhan@email.com>
2019-09-10 18:19:48 +0700

    Update

--- ber-app/_frontend/views/v_contentberitaall.php
+++ ber-app/_frontend/views/v_contentberitaall.php
@@ -4,7 +4,7 @@
         		<div class="container">
         			<div class="row">
         				<div class="page-title">
-        					<h2 class="title">Welcome to my blog</h2>
+							<h2 class="title">Welcome to my blog</h2>
         				</div>
         				<div id="page-breadcrumbs">
         					<nav class="breadcrumb-trail breadcrumbs">
@@ -24,17 +24,36 @@
         			<div class="row">
         				<div class="blog">
         					<div class="main-content">
+							<?php
+    if (count($artikel)) {
+	?>
+<?php $no=1;
+foreach($artikel as $key => $row){ 
+	$isi=strip_tags($row['isi_berita']);
+	$isi=substr($isi,0,250); 
+	$judul=seo_link($row['judul']);
+	$photopath = str_replace('-', '/', $row['tanggal_modif']);				
+	$a=substr($row['tanggal'], 0,4);
+	$b=substr($row['tanggal'], 5,2);
+	$c=substr($row['tanggal'], 8,9);
+	$tanggal=$c.'/'.$b.'/'.$a;
 
+	if  ($row['gambar']!='') { 
+		$gambar=base_url() ."foto_berita/".$photopath."/small_".$row['gambar'];
+	} else { 
+		$gambar=base_url() ."foto_berita/image-default.jpg";
+	}
+?>
         						<article class="post">
         							<div class="entry-wrapper">
         								<div class="entry-cover">
-        									<a href="blog-single.html">
-        										<img src="images/blog/1.jpg" alt="images">
-        									</a>
+        										<img src="<?php echo $gambar; ?>" alt="images" style="height: 394.3px; width: 525.75px;">
         								</div><!-- /.entry-cover -->
         								<div class="entry-header">
         									<span class="entry-time">January 26, 2016</span>
-        									<h2 class="entry-title"><a href="blog-single.html">3 Reasons Your Business Needs A Budget Now</a></h2>
+        									<h2 class="entry-title">
+												<a href="<?php echo base_url(); ?>berita/detail/<?php echo $row['id_berita']."/".$judul."/";?>"><?php echo $row['judul']; ?></a>
+											</h2>
         									<div class="entry-meta">
         										<span>By</span>
         										<span class="entry-author">
@@ -46,92 +65,16 @@
         									</div><!-- /.entry-meta -->
         								</div><!-- /.entry-header -->
         								<div class="entry-content">
-        									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer too...
-        									<div class="readmore"><a href="blog-single.html" class="more-link">Continue Read</a></div>
+										<?php echo $isi; ?>...
+										<div class="readmore"><a href="blog-single.html" class="more-link">Selengkapnya</a></div>
         								</div><!-- /.entry-content -->
         							</div><!-- /.entry-wrapper -->
 								</article><!-- /.post -->
 								
-        						<article class="post">
-        							<div class="entry-wrapper">
-        								<div class="entry-cover">
-        									<a href="blog-single.html">
-        										<img src="images/blog/2.jpg" alt="images">
-        									</a>
-        								</div><!-- /.entry-cover -->
-        								<div class="entry-header">
-        									<span class="entry-time">January 26, 2016</span>
-        									<h2 class="entry-title"><a href="blog-single.html">2016 Queensland Small Business Week</a></h2>
-        									<div class="entry-meta">
-        										<span>By</span>
-        										<span class="entry-author">
-        											<a href="#" class="entry-author-link">themesflat</a>
-        										</span>
-        										<span>in</span>
-        										<span class="entry-categories"><a href="#">Business</a></span>
-        										<span class="entry-comments-link"><a href="#">0</a></span>
-        									</div><!-- /.entry-meta -->
-        								</div><!-- /.entry-header -->
-        								<div class="entry-content">
-        									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer too...
-        									<div class="readmore"><a href="blog-single.html" class="more-link">Continue Read</a></div>
-        								</div><!-- /.entry-content -->
-        							</div><!-- /.entry-wrapper -->
-								</article><!-- /.post -->
-								
-        						<article class="post">
-        							<div class="entry-wrapper">
-        								<div class="entry-cover">
-        									<a href="blog-single.html">
-        										<img src="images/blog/3.jpg" alt="images">
-        									</a>
-        								</div><!-- /.entry-cover -->
-        								<div class="entry-header">
-        									<span class="entry-time">January 26, 2016</span>
-        									<h2 class="entry-title"><a href="blog-single.html">The Tax Office doesn’t always get it right</a></h2>
-        									<div class="entry-meta">
-        										<span>By</span>
-        										<span class="entry-author">
-        											<a href="#" class="entry-author-link">themesflat</a>
-        										</span>
-        										<span>in</span>
-        										<span class="entry-categories"><a href="#">Business</a></span>
-        										<span class="entry-comments-link"><a href="#">0</a></span>
-        									</div><!-- /.entry-meta -->
-        								</div><!-- /.entry-header -->
-        								<div class="entry-content">
-        									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer too...
-        									<div class="readmore"><a href="blog-single.html" class="more-link">Continue Read</a></div>
-        								</div><!-- /.entry-content -->
-        							</div><!-- /.entry-wrapper -->
-        						</article><!-- /.post -->
-        						<article class="post">
-        							<div class="entry-wrapper">
-        								<div class="entry-cover">
-        									<a href="blog-single.html">
-        										<img src="images/blog/4.jpg" alt="images">
-        									</a>
-        								</div><!-- /.entry-cover -->
-        								<div class="entry-header">
-        									<span class="entry-time">January 26, 2016</span>
-        									<h2 class="entry-title"><a href="blog-single.html">The fastest way to grow your business</a></h2>
-        									<div class="entry-meta">
-        										<span>By</span>
-        										<span class="entry-author">
-        											<a href="#" class="entry-author-link">themesflat</a>
-        										</span>
-        										<span>in</span>
-        										<span class="entry-categories"><a href="#">Business</a></span>
-        										<span class="entry-comments-link"><a href="#">1</a></span>
-        									</div><!-- /.entry-meta -->
-        								</div><!-- /.entry-header -->
-        								<div class="entry-content">
-        									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer too...
-        									<div class="readmore"><a href="blog-single.html" class="more-link">Continue Read</a></div>
-        								</div><!-- /.entry-content -->
-        							</div><!-- /.entry-wrapper -->
-        						</article><!-- /.post -->
-
+								<?php $no=$no+1; } ?>
+  <?php } else { ?>
+<h4 >Maaf, Data Belum Tersedia !</h4>
+<?php } ?>
         						<!-- MULAI NAVIGASI -->
         						<?php echo $pagination; ?>
 								<!-- SELESAI NAVIGASI -->
