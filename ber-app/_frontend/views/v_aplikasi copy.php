29f019c6d6a672c0b755a2c88f0464f98078fb80
Muh. Ramadhan <muh.ramadhan@email.com>
2019-09-11 03:14:08 +0700

    Update

--- ber-app/_frontend/views/v_aplikasi copy.php
+++ ber-app/_frontend/views/v_aplikasi copy.php
@@ -1,22 +1,5 @@
 
 
-	
-		
-
-		<div class="col-xs-6 col-sm-3 col-md-6 quick-link flag-requests">
-
-			<a href="">
-
-				<span class="icon"></span>
-
-				<span class="title">SIPKD</span>
-
-			</a>
-
-		</div>
-
-	
-
 		
 
 		
