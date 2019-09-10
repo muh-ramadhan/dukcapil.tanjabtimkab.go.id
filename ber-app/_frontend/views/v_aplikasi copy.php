ba457a391e306b859cf850e09ad77a19efe3c49d
Muh. Ramadhan <muh.ramadhan@email.com>
2019-09-11 03:14:40 +0700

    Update

--- ber-app/_frontend/views/v_aplikasi copy.php
+++ ber-app/_frontend/views/v_aplikasi copy.php
@@ -1,35 +1,6 @@
 
 
-		
-
-		<div class="col-xs-6 col-sm-3 col-md-6 quick-link internships">
-
-			<a href="">
-
-				<span class="icon"></span>
-
-				<span class="title">Sistem Informasi Pelayanan Terpadu</span>
-
-			</a>
-
-		</div>
-
-		<!--
-
-		<div class="col-xs-6 col-sm-3 col-md-6 quick-link visiting-d-c">
-
-			<a href="">
-
-				<span class="icon"></span>
-
-				<span class="title">Visiting D.C.</span>
-
-			</a>
-
-		</div>
-
 	
-
 		
 
 		
