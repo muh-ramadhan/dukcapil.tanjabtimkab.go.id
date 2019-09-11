ea2131bace100d7ed6d2cf2037b76414a306aa2d
Muh. Ramadhan <muh.ramadhan@email.com>
2019-09-11 10:58:05 +0700

    Update

--- ber-app/_frontend/views/v_contentartikel.php
+++ ber-app/_frontend/views/v_contentartikel.php
@@ -72,118 +72,52 @@
  		</div>
  	</div>
  <?php
-
 	}
-
 	//---- KONDISI BILA KATEGORI BERIT ------------
-
 	// && ($this->uri->segment(2,0)==null || $this->uri->segment(2,0)=='index')
-
-
-
-
-
 	else if ($this->uri->segment(1, 0) == 'kategori') {
-
 		?>
 
-
-
  	<div class="content grid_7 allartikel marked-category">
-
  		<div class="single-page">
-
  			<h4 class="rs title-box-outside">Kategori <?php echo $judulan; ?></h4><br>
-
-
-
  			<?php
-
 					if (count($artikel)) {
-
 						?>
-
-
-
  				<?php
-
 							$no = 1;
-
 							foreach ($artikel as $key => $row) {
-
 								$isi = strip_tags($row['isi_artikel']);
-
 								$isi = substr($isi, 0, 220);
-
 								$judul = seo_link($row['judul']);
-
 								$photopath = str_replace('-', '/', $row['tanggal_modif']);
-
 								$a = substr($row['tanggal'], 0, 4);
-
 								$b = substr($row['tanggal'], 5, 2);
-
 								$c = substr($row['tanggal'], 8, 9);
-
 								$tanggal = $c . '/' . $b . '/' . $a;
-
-
-
 								if ($row['gambar'] != '') {
-
 									$gambar = base_url() . "foto_artikel/" . $photopath . "/small_" . $row['gambar'];
 								} else {
-
 									$gambar = base_url() . "foto_artikel/image-default.jpg";
-								}
-
-								?>
-
+								} ?>
  					<div class="content-info-short clearfix">
-
  						<a href="<?php echo base_url(); ?>artikel/detail/<?php echo $row['id_artikel'] . "/" . $judul . "/"; ?>" class="thumb-img">
-
  							<div class="thumb-news1" data-original="<?php echo $gambar; ?>" style="background-image: url('<?php echo $gambar; ?>')"></div>
-
  						</a>
-
  						<div class="wrap-short-detail">
-
  							<h3 class="rs acticle-title"><a class="be-fc-orange" href="<?php echo base_url(); ?>artikel/detail/<?php echo $row['id_artikel'] . "/" . $judul . "/"; ?>"><?php echo $row['judul']; ?></a></h3>
-
  							<p class="rs tiny-desc"><?php echo nama_hari($row['tanggal']) . ', '; ?> <span class="fw-b fc-gray"><?php echo tgl_indo($row['tanggal']) . ' | ';
 																																				echo $row['jam'] . ' WIB ';
 
 																																				?></span></p>
-
  							<p class="rs title-description"><?php echo $isi; ?>...</p>
-
  						</div>
-
  					</div>
-
- 				<?php
-
-								$no = $no + 1;
-							}
-
-							?>
-
-
-
- 			<?php
-
-					} else {
-
-						?>
-
+ 				<?php $no = $no + 1;
+							} ?>
+ 			<?php } else { ?>
  				<h4>Maaf, Data Belum Tersedia !</h4>
-
- 			<?php
-
-					}
-
-					?>
+ 			<?php } ?>
 
  			<div class="clearfix"></div>
 
